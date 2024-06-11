9f98d72d468a66e6a7b631b46b6c604afa799841
Elliot <elliot@codeaura.ai>
2024-06-11 14:54:44 +0400

    Create User model with firstName and lastName attributes.

---
+++ api/models/User.js
@@ -0,0 +1,18 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+module.exports = {
+  attributes: {
+    firstName: {
+      type: 'string',
+      required: true
+    },
+    lastName: {
+      type: 'string',
+      required: true
+    }
+  }
+};
